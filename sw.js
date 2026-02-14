676ce1226673a8d436300c0f0f148c1e7fe9ca43
kilitest <kilu._@hotmail.com>
2026-02-14 08:54:04 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = "morning-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./db.js",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./sounds/soft.mp3"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil((async () => {
+    const c = await caches.open(CACHE);
+    await c.addAll(ASSETS);
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith((async () => {
+    const cached = await caches.match(e.request);
+    if (cached) return cached;
+    try {
+      return await fetch(e.request);
+    } catch {
+      return caches.match("./index.html");
+    }
+  })());
+});
